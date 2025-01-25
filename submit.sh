98dd49b912f38127d78f5024e67071dd00db7a5e
Moonlight135 <8eliana1@gmail.com>
2025-01-24 21:59:34 -0800

    changed work to be visible in .gitignore file

--- submit.sh
+++ submit.sh
@@ -6,7 +6,7 @@ rm -rf submit submit.zip
 mkdir -p submit
 
 # submit team.txt
-printf "Victor Zhong,vzhong\nNoah Smith,nasmith\nSanta Claus,sclaus" > submit/team.txt
+printf "Eliana Dietrich,eliana1\nJay Bhateja,jbhateja\nAndy Ruan,yhruan22" > submit/team.txt
 
 # train model
 python src/myprogram.py train --work_dir work
